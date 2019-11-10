c1c5e40d458b1358adc8245e2677148c32eafb17
Ezgi Siir Kibris <ezgisiir@sabanciuniv.edu>
2019-11-09 20:54:05 -0400

    Building data structure.

--- test.py
+++ test.py
@@ -3,7 +3,13 @@ class TreeNode:
         self.value = value
         self.freq = freq
         self.parent = parent
-        self.next = None
+        self.next = []
+
+
+def add_child_to_parent(parent, value):
+    word_node = TreeNode(value, 1, parent)
+    parent.next.append(word_node)
+    return word_node
 
 
 class Tree:
@@ -11,23 +17,39 @@ class Tree:
     def __init__(self):
         self.root = TreeNode(None, None, None)
 
-    def count(self):
-        count = 1
+    def add_words(self, words_to_add):
         node = self.root
-        while node.next is not None:
-            count = count + 1
-            node = node.next
-        return count
+        children = node.next
 
-    def add(self, tree_node):
-        node = self.root
-        next_node = node.next
-        while next_node is not None:
-            node = next_node
-            next_node = node.next
-        node.next = tree_node
+        for word in words_to_add:
+            matching_node = None
+            if children is None or len(children) == 0:
+                matching_node = add_child_to_parent(node, word)
+            else:
+                child_found = False
+                for child in children:
+                    if child.value == word:
+                        matching_node = child
+                        child.freq = child.freq + 1
+                        child_found = True
+
+                if not child_found:
+                    matching_node = add_child_to_parent(node, word)
+
+            node = matching_node
+            children = node.next
 
 
 tree = Tree()
 
-print(tree.count())
+words = ['K', 'E', 'M', 'O', 'Y']
+tree.add_words(words)
+
+words = ['K', 'E', 'O', 'Y']
+tree.add_words(words)
+
+words = ['K', 'E', 'M']
+tree.add_words(words)
+
+
+print('Gokhan')
