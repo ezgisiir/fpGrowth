8fe4e7721e1a9b750fb758b34f5c02c1329641e2
Gokhan Ozgozen <gokhan.ozgozen@gmail.com>
2019-11-10 03:25:07 +0300

    Tree ds.

--- test.py
+++ test.py
@@ -3,3 +3,31 @@ class TreeNode:
         self.value = value
         self.freq = freq
         self.parent = parent
+        self.next = None
+
+
+class Tree:
+
+    def __init__(self):
+        self.root = TreeNode(None, None, None)
+
+    def count(self):
+        count = 1
+        node = self.root
+        while node.next is not None:
+            count = count + 1
+            node = node.next
+        return count
+
+    def add(self, tree_node):
+        node = self.root
+        next_node = node.next
+        while next_node is not None:
+            node = next_node
+            next_node = node.next
+        node.next = tree_node
+
+
+tree = Tree()
+
+print(tree.count())
